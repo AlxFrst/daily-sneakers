f05e1c9ca1c70f49b4f1aa974bdd5cc6d36e1780
Alexandre Forestier <69115925+AlxFrst@users.noreply.github.com>
2022-06-01 17:50:05 +0200

    seeds en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,43 +2,26 @@
 Sneaker.destroy_all
 Price.destroy_all
 
-# Sneakers to database
-page = 1
-url = "https://www.klekt.com/all?page=#{page.to_s}"
-3.times do |i|
-  require "Nokogiri"
-  require "open-uri"
+# Sneakers database
+require "Nokogiri"
+require "open-uri"
 
-  doc = Nokogiri::HTML(URI.open(url))
-  doc.css(".pod-link").each do |item|
-    link = item.attributes["href"].value
-    detail_link = "https://www.klekt.com/#{link}"
-    detail_doc = Nokogiri::HTML(URI.open(detail_link))
-    details = detail_doc.css(".c-pdp-info")
-    sku = details.css("span")[1].text
-    brand = detail_doc.css(".u-letter-spacing--normal")[2].text
-    model = detail_doc.css(".u-title")[0].text.gsub(brand, "").strip.remove("\n").remove("\t")
-    #get year in model like model (2000)
-    year = model.match(/\d{4}/)[0]
-    model = model.gsub(year, "").strip.remove("\n").remove("\t").remove("(").remove(")").strip
+# UPCOMING RELEASE
+coming_pages = 1
+coming_url = "https://moresneakers.com/releases?group=in_stock&page=#{coming_pages.to_s}"
 
-    sizes = detail_doc.css(".c-price-point")
-    sizes.each do |size|
-      size_name = size.css("span")[0].text.remove("US").remove("\t").remove("\r").strip
-      size_price = size.css("span")[2].text.remove("€")
-      p "--------------------------"
-      p " #{brand} #{model} #{year} #{sku} #{size_name.to_f}"
-      if Sneaker.where(reference: sku) == []
-        sneaker = Sneaker.new(brand: brand, model: model, year: year, reference: sku)
-        sneaker.save!
-      else
-        sneaker = Sneaker.where(reference: sku)[0]
-      end
-      p sneaker.id
-      price = Price.new(timestamp: Time.now, price: size_price.to_f, size: size_name.to_f, market: 'Klekt')
-      price.sneakers_id = sneaker.id
-      price.save!
-    end
-  end
-  page += 1
+coming_doc = Nokogiri::HTML(URI.open(coming_url))
+
+coming_doc.css(".releases-container .product-card").each do |product|
+  link = "https://moresneakers.com" + product.css("a").attr("href").value
+  page = Nokogiri::HTML(URI.open(link))
+  details = page.css("section")
+  brand = details.css("p").css("a")[0].text
+  model = details.css("p").css("a")[1].text
+  colors = details.css("p")[2].css("strong").text
+  sku = details.css("p")[3].css("strong").text
+  gender = details.css("p")[5].css("strong").text
+  p img_url = "lol"
 end
+
+# ALREADY RELEASED
