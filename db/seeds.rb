0447dc8409edc934dc7adf4b78a68868a3da095a
Alexandre Forestier <69115925+AlxFrst@users.noreply.github.com>
2022-06-01 16:46:35 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,6 @@
 #Create size table
 Sneaker.destroy_all
 Price.destroy_all
-<<<<<<< HEAD
-sneakers_us_sizes = [3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 15, 16, 17, 18]
-sneakers_eu_sizes = [35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 54.5, 55, 56, 57, 58, 59]
-sneakers_uk_sizes = [2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 14, 15, 16, 17]
-
-p "#{sneakers_us_sizes.length} US sizes | #{sneakers_eu_sizes.length} EU sizes | #{sneakers_uk_sizes.length} UK sizes"
-
-sneakers_us_sizes.length.times do |i|
-  Size.create(US: sneakers_us_sizes[i], EU: sneakers_eu_sizes[i], UK: sneakers_uk_sizes[i])
-  p "Created size US: #{sneakers_us_sizes[i]} | EU: #{sneakers_eu_sizes[i]} | UK: #{sneakers_uk_sizes[i]}"
-end
-=======
->>>>>>> master
 
 # Sneakers to database
 page = 1
@@ -41,13 +28,6 @@ url = "https://www.klekt.com/all?page=#{page.to_s}"
       size_price = size.css("span")[2].text.remove("€")
       p "--------------------------"
       p " #{brand} #{model} #{year} #{sku} #{size_name.to_f}"
-<<<<<<< HEAD
-      sneaker = Sneaker.new(brand: brand, model: model, year: year, reference: sku)
-      sneaker.size = Size.where(US: size_name.to_f).first
-      sneaker.save!
-      #price = Price.new(timestamp: Time.now, price: size_price, market: "Klekt", sneakers_id: sneaker.id)
-      #price.save!
-=======
       if Sneaker.where(reference: sku) == []
         sneaker = Sneaker.new(brand: brand, model: model, year: year, reference: sku)
         sneaker.save!
@@ -58,7 +38,6 @@ url = "https://www.klekt.com/all?page=#{page.to_s}"
       price = Price.new(timestamp: Time.now, price: size_price.to_f, size: size_name.to_f, market: 'Klekt')
       price.sneakers_id = sneaker.id
       price.save!
->>>>>>> master
     end
   end
   page += 1
