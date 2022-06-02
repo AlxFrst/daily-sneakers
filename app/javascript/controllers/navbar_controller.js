f4fcdb4c7e7cc090bc6aac576ae685f59578d1d2
julien Diet <juliendiet7@gmail.com>
2022-06-02 17:43:07 +0200

    front de fou

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   updateNavbar() {
-    if (document.body.scrollTop >= 80 || document.documentElement.scrollTop >= 80) {
+    if (document.body.scrollTop >= 10 || document.documentElement.scrollTop >= 10) {
       this.element.classList.add("navbar-lewagon-white")
     } else {
       this.element.classList.remove("navbar-lewagon-white")
