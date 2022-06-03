327897e42853ca361821892eaeab2f3399ee84a8
Olivier <olivier.gomb+github@gmail.com>
2022-06-03 09:12:53 +0200

    correctif gsub

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,17 +1,17 @@
 class SneakersController < ApplicationController
   def index
-    if params[:brands] != "" && params[:sellers] != ""
+    if params[:brands] != "" && params[:sellers] != "" && params[:brands] && params[:sellers]
       query = "brand IN ('#{params[:brands].gsub(",", "','")}')
       AND prices.market IN ('#{params[:sellers].gsub(",", "','")}')
       AND prices.price BETWEEN #{params[:min]} AND #{params[:max]}"
       @sneakers = Sneaker.joins(:prices).where(query).group(:id)
-    elsif params[:brands] != ""
+    elsif params[:brands] != "" && params[:brands]
       @sneakers = Sneaker.joins(:prices).where("brand IN ('#{params[:brands].gsub(",", "','")}')
       AND prices.price BETWEEN #{params[:min].to_i} AND #{params[:max].to_i}").group(:id)
-    elsif params[:sellers] != ""
+    elsif params[:sellers] != "" && params[:sellers]
       @sneakers = Sneaker.joins(:prices).where("prices.market IN ('#{params[:sellers].gsub(",", "','")}')
       AND prices.price BETWEEN #{params[:min].to_i} AND #{params[:max].to_i}").group(:id)
-    elsif params[:min] != ""
+    elsif params[:min] != "" && params[:min]
       @sneakers = Sneaker.joins(:prices).where("prices.price BETWEEN #{params[:min].to_i} AND #{params[:max].to_i}").group(:id)
     else
       @sneakers = Sneaker.all
