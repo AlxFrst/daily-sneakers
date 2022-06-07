666e33e563dc2e859a593d690e2c94a796c6518f
Alexandre Forestier <69115925+AlxFrst@users.noreply.github.com>
2022-06-07 09:31:57 +0200

    Correctif

--- app/models/whishlist.rb
+++
@@ -1,4 +0,0 @@
-class Whishlist < ApplicationRecord
-  belongs_to :sneakers
-  belongs_to :users
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :sneakers, only: [:index, :show]
+  post "/sneakers/:id/add_to_wishlist", to: "sneakers#add_to_wishlist", as: "add_to_wishlist"
   get "wishlist", to: "dashboards#wishlist"
   get "collection", to: "dashboards#collection"
   get "profile", to: "dashboards#profile"
