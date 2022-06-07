79f6963c0507d26643a991e91508714f90a78333
Alexandre Forestier <69115925+AlxFrst@users.noreply.github.com>
2022-06-07 14:50:25 +0200

    Finish ?

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,20 @@
 class DashboardsController < ApplicationController
   def wishlist
     @sneaker = Wishlist.where(user: current_user)
+    total = 0
+    @sneaker.each do |sneaker|
+      total += Price.where(sneaker: sneaker.sneaker).last.price
+    end
+    @total = total
   end
 
   def collection
     @sneaker = Collection.where(user: current_user)
+    total = 0
+    @sneaker.each do |sneaker|
+      total += Price.where(sneaker: sneaker.sneaker).last.price
+    end
+    @total = total
   end
 
   def profile
