e32b4d2f762f3b954c164e8192856b9e2cae4ce5
Alexandre Forestier <69115925+AlxFrst@users.noreply.github.com>
2022-06-07 10:26:54 +0200

    avancement

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :sneakers, only: [:index, :show]
-  post "/sneakers/:id/add_to_wishlist", to: "sneakers#add_to_wishlist", as: "add_to_wishlist"
+  resources :sneakers, only: [:index, :show] do
+    resources :wishlists, only: [:new, :create]
+    resources :collections, only: [:new, :create]
+  end
   get "wishlist", to: "dashboards#wishlist"
   get "collection", to: "dashboards#collection"
   get "profile", to: "dashboards#profile"
